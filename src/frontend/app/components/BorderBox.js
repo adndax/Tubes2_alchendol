16b6aeca47be3956f1cdd6aafbd06cffba2d1577
adndax <adindaputri0517@gmail.com>
2025-05-02 11:09:36 +0700

    feat: create ElementBox

--- src/frontend/app/components/BorderBox.js
+++ src/frontend/app/components/BorderBox.js
@@ -1,5 +1,7 @@
 "use client";
+import Image from "next/image";
 import { cn } from "@utils";
+import { Subheading } from "./Typography";
 
 export const BorderBox = ({ children, className, height }) => (
   <div className="w-full flex justify-center">
@@ -15,3 +17,12 @@ export const BorderBox = ({ children, className, height }) => (
     </div>
   </div>
 );
+
+export const ElementBox = ({ name, imageSrc }) => {
+  return (
+    <div className="flex flex-col items-center justify-center border border-secondary rounded-md p-2 w-29 h-29 bg-background gap-3">
+      <Image src={imageSrc} alt={name} width={40} height={40} />
+      <Subheading>{name}</Subheading>
+    </div>
+  );
+};
