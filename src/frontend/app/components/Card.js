2b598c92c32f441e7b7517a60afa2500f9aceb26
adndax <adindaputri0517@gmail.com>
2025-05-04 10:15:45 +0700

    feat: create cards for result page

--- src/frontend/app/components/Card.js
+++ src/frontend/app/components/Card.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Subheading, SubheadingRed, DescriptionBlack, DescriptionWhite,
+import { Subheading, SubheadingRed, DescriptionBlack, DescriptionWhite, DescriptionRed
 } from "./Typography";
 
 export const SpellCard = ({ imageSrc, title, subtitle, isSelected, onClick }) => {
@@ -25,14 +25,42 @@ export const SpellCard = ({ imageSrc, title, subtitle, isSelected, onClick }) =>
   );
 };
 
-export const ElementsCard = ({ name, imageSrc, onClick }) => {
+export const ElementsCard = ({ name, imageSrc, className = "" }) => {
   return (
     <div
-      onClick={onClick}
-      className="bg-secondary rounded-xl shadow-md p-4 w-[110px] h-[110px] flex flex-col items-center justify-center gap-3"
+      className={`bg-secondary rounded-xl shadow-md p-4 w-[110px] h-[110px] flex flex-col items-center justify-center gap-3 ${className}`}
     >
       <Image src={imageSrc} alt={name} width={40} height={40} />
       <Subheading className="text-primary">{name}</Subheading>
+    </div>
+  );
+};
+
+export const ResultStatCard = ({ iconSrc, value, label, className = "" }) => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+        <Subheading className="text-secondary">{label}</Subheading>
+      <div
+        className={`bg-secondary text-center rounded-xl p-4 w-[130px] h-[140px] shadow-md border border-secondary flex flex-col items-center justify-between ${className}`}
+      >
+        <div className="h-[60px] flex items-center justify-center">
+          <Image src={iconSrc} width={60} height={60} alt="Stat Icon" />
+        </div>
+
+        <div className="w-[130px] h-[2px] bg-primary" />
+
+        <DescriptionRed>{value}</DescriptionRed>
+      </div>
+    </div>
+  );
+};
+
+export const SearchTreeBox = ({ treeSrc }) => {
+  return (
+    <div className="bg-secondary rounded-xl p-6 w-full flex justify-center shadow-md">
+      <div className="bg-background p-4 rounded">
+        <Image src={treeSrc} alt="Tree Result" width={400} height={250} />
+      </div>
     </div>
   );
 };
